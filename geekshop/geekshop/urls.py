05b7ea1d8a60a3622c9ab1329972f48772d2c68b
akryilov <ka_dikk@mail.ru>
2022-03-28 21:00:57 +0300

    add homework for lesson 3

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import settings, static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -26,4 +28,6 @@ urlpatterns = [
 
 ]
 
-# urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_URL)
+urlpatterns += static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
+
+# urlpatterns +=static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
--- geekshop/mainapp/models.py
+++ geekshop/mainapp/models.py
@@ -1,21 +1,27 @@
+''' models for product and categories '''
 from django.db import models
 
+
 # Create your models here.
-# class ProductCategories(models.Model):
-#
-#     name = models.CharField(max_length=64, unique=True)
-#     descriptions = models.TextField(blank=True, null=True)
-#
-# def__str__(self):
-# return self.name
-#
-# class Product(models.Model):
-#
-#     name = models.CharField(max_lenght = 128)
-#     image = models.ImageField(upload_to='product_images',blank=True)
-#     description = models.TextField(blank = True, null = True)
-#     price = models.DecimalField(max_digits=8,decimal_places=2)
-#     quantity = models.PositiveIntegerFelld(default = 8)
-#     category = models.ForeignKey(ProductCategories, on_delete = models.)
-#
-#     def__str__(self):
+class ProductCategories(models.Model):
+    ''' model for category '''
+
+    name = models.CharField(max_length=64)
+    descriptions = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    ''' model for product '''
+
+    name = models.CharField(max_length=128)
+    image = models.ImageField(upload_to='product_images', blank=True)
+    descriptions = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=8)
+    category = models.ForeignKey(ProductCategories, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name} | {self.category}'
