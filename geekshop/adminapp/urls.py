0a1ac4a27d5c1b64fa9dd037eb8d0f677f736fcf
akryilov <ka_dikk@mail.ru>
2022-04-19 18:20:07 +0300

    Commint CBV implementation started

--- geekshop/adminapp/urls.py
+++ geekshop/adminapp/urls.py
@@ -1,23 +1,23 @@
 from django.urls import path
 
-from adminapp.views import admin_users, index, admin_user_create, admin_user_delete, admin_user_update
-
+from adminapp.views import UserListView, IndexTemplateView, UserCreateView, UserUpdateView, UserDeleteView
+from adminapp.views import CategoryListView
 ###
 # import adminapp.views as adminapp
-from adminapp.views import admin_categories, admin_products, admin_category_create, admin_category_update, admin_category_delete, admin_product_create, admin_product_delete, admin_product_update
+from adminapp.views import admin_products, admin_category_create, admin_category_update, admin_category_delete, admin_product_create, admin_product_delete, admin_product_update
 
 app_name = 'adminapp'
 
 urlpatterns = [
 
-    path('', index, name='index'),
-    path('users/', admin_users, name = 'admin_users'),
-    path('user-create/', admin_user_create, name='admin_user_create'),
-    path('user-update/<int:id>/', admin_user_update, name='admin_user_update'),
-    path('user-delete/<int:id>/', admin_user_delete, name='admin_user_delete'),
+    path('', IndexTemplateView.as_view(), name='index'),
+    path('users/', UserListView.as_view(), name = 'admin_users'),
+    path('user-create/', UserCreateView.as_view(), name='admin_user_create'),
+    path('user-update/<int:id>/', UserUpdateView.as_view(), name='admin_user_update'),
+    path('user-delete/<int:id>/', UserDeleteView.as_view(), name='admin_user_delete'),
 
 
-    path('categories/', admin_categories, name ='admin_categories'),
+    path('categories/', CategoryListView.as_view(), name ='admin_categories'),
     path('category-create/', admin_category_create, name ='admin_category_create'),
     path('category-update/<int:id>/', admin_category_update, name ='admin_category_update'),
     path('category-delete/<int:id>/', admin_category_delete, name ='admin_category_delete'),
